988d04699786446fab1c35a117ab8ca6e0b09532
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-08 21:19:33 -0800

    fixing things

--- Develop/index.js
+++ Develop/index.js
@@ -100,39 +100,3 @@ inquirer
   });
   
     
-
-// // TODO: Include packages needed for this application
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
-
-
-
-
-
-
-
-  // .then(answers => {
-  //     const { input, description ,name, location, linkd, githubname } = answers;
-  //     console.log(answers)
-  
-  //     fs.writeFile("README.md", READMEfile, err => {
-  //     if(err){
-  //         console.error(err);
-  //     }else{
-  //         console.log("Great!");
-  //     }
-      
-  // });
-  
-  // });
