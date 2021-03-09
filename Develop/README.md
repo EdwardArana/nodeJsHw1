17e58856cb3948fe5872693f37c635d458f60ecc
Edward Arana <Edwardaranadlt@gmail.com>
2021-03-09 15:28:09 -0800

    updating

---
+++ Develop/README.md
@@ -0,0 +1,21 @@
+# Edward
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Description
+ARana
+## Installation
+ADAS
+## Usage
+ASDASDAS
+## License 
+This application has the following License: MIT
+## Contributing
+ASDASDAS
+## Tests
+ASDASDA
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // if (license !== 'None') {
-  //   return `![GitHub license](https://img.shields.io/badges/license-MIT-blue.svg)`
-  // }
-  // return '';
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/github/license/EdwardArana/nodeJsHw1)`
+  }
+  return '';
 }
 
 // TODO: Create a function that returns the license link
